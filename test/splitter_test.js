02dfd80256a75dafcfc9e7e2962bace85377e77d
Wojciech Kalinski <wojciech.kalinski@espeo.eu>
2018-10-09 12:53:24 +0200

    Another attempt in order to make tests working

--- test/splitter_test.js
+++ test/splitter_test.js
@@ -8,10 +8,6 @@ const Splitter = artifacts.require("./Splitter.sol");
 Promise.promisifyAll(web3.eth, { suffix: "Promise" });
 
 contract("Splitter contract", (accounts) => {
-    const MAX_GAS = 30000;
-    const AMOUNT = 0.0000007;
-
-    console.log(accounts);
 
     let alice;
     let bob;
@@ -19,31 +15,41 @@ contract("Splitter contract", (accounts) => {
 
     [alice, bob, carol] = accounts;
 
-    let ownerBalance;
-
-
     let contractInstance;
-    beforeEach("Checking if smart contract is setup properly -  accounts", async () => {
+    before("Checking if smart contract is setup properly -  accounts", async () => {
         assert.isAtLeast(accounts.length, 3, "not enough, something is wrong here....");
 
-        console.log("Owner: " + accounts[0]);
+        console.log("Owner: " + alice);
         console.log("Bob: " + bob);
         console.log("Carol: " + carol);
 
         contractInstance = await Splitter.new(bob, carol)
         return web3.eth.getBalance(alice)
             .then(_balance => {
-                ownerBalance = _balance;
-                console.log("Alice balance: " + ownerBalance);
+                console.log("Alice balance: " + _balance);
             });
     });
 
-    it("Should check if any pre-condition thing is working properly", async () => {
-        assert.isTrue(accounts.length >= 3, accounts.length + " accounts, required 3. Something ain't right.");
+    it("Should split ether accordingly",  done => {
+        //when
+         contractInstance.sendEther.call({ value: 5000000, from: alice})
+        //...now what
+            .then(success => {
+                assert.isTrue(success, "failed to do something.");
+                return contractInstance.sendEther({value: 5000000, from: alice});
+            })
+            .then(resultValue => {
+                assert.equal(resultValue.toString(10), "2", "one does not simply walk into a mordor.");
+                done();
+            })
+            .catch(done);
     });
 
-    it("Should split ether accordingly", () => {
+    it("Should not split ether if value is 0", async () => {
+        //when
+        await contractInstance.sendEther({ value: 0, from: alice});
 
-        contractInstance.splitEther();
-    });
+        //then
+        assert.fail();
+    })
 });
